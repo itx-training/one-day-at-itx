2f1c581888dbba73d041af6f50e80e0ba3a39791
Guenter Schafranek <gschafra@web.de>
2021-05-04 11:27:55 +0200

    Node.js 12 via nvm, entry and test page/s

--- .gitpod.dockerfile
+++ .gitpod.dockerfile
@@ -9,21 +9,15 @@ RUN a2dismod mpm_event
 
 RUN apt-get update && apt-get upgrade -y && apt-get install -y mysql-server php-xmlrpc php-pdo-mysql
 
-# Install Node.js
-RUN apt-get install -y curl ca-certificates apt-transport-https --no-install-recommends && \
-	curl -sL https://deb.nodesource.com/setup_${NODEJS_VERSION_MAJOR}.x | bash - \
-    && apt-get update \
-	&& apt-get install -y nodejs
-	
 # Install Yarn
 RUN mkdir -p /var/cache/yarn
 RUN apt install -y gnupg2
 RUN	curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - && \
     echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list && \
     apt-get update && apt install -y yarn=${YARN_VERSION}
-	
+
 # Mark packages as hold, i.e. staying at given version
-RUN apt-mark hold yarn 
+RUN apt-mark hold yarn
 
 RUN echo "include \${GITPOD_REPO_ROOT}/gitpod_config/apache/apache.conf" > /etc/apache2/apache2.conf
 RUN echo ". \${GITPOD_REPO_ROOT}/gitpod_config/apache/envvars" > /etc/apache2/envvars
--- index.php
+++ index.php
@@ -1,2 +1,35 @@
-<?php
-    phpinfo();
+<!doctype html>
+<html lang="de">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Willkommen bei it.x</title>
+</head>
+<body>
+	<div>
+		<h1>Ein Tag bei it.x</h1>
+		<p>
+			Liebe/r Bewerber*in/Praktikant*in,
+
+			wir freuen uns, dass du dich entschieden hast, bei uns einen Tag zu verbringen, um uns besser kennen zu lernen und uns
+			zu zeigen, was du so "drauf hast". Um letzteres einigermaßen geregelt überprüfen zu können, haben wir folgende, kleine Aufgaben vorbereitet:
+
+			<ol>
+				<li><a href="docs/aufgabe_php.html">Fachtest Programmierung PHP</a></li>
+				<li><a href="docs/aufgabe_js_ajax.html">Fachtest Programmierung JS/AJAX</a></li>
+				<li><a href="docs/aufgabe_html_css.html">Fachtest Programmierung HTML/CSS</a></li>
+			</ol>
+
+			Bitte scheue dich nicht bei Fragen, z.B. zur Bearbeitung der Aufgaben aufkommen aber auch zu uns, auf uns zuzukommen.
+
+			Viel Erfolg!
+		</p>
+	</div>
+	<div>
+		<?php
+			phpinfo();
+		?>
+	</div>
+</body>
+</html>
